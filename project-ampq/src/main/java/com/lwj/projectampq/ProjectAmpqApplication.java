56aa9083c7fc3e3a3e3d5e55adc77d9c322938b9
Lwj1423 <m17621954542@163.com>
2019-07-03 16:14:56 +0800

    first commit

---
+++ project-ampq/src/main/java/com/lwj/projectampq/ProjectAmpqApplication.java
@@ -0,0 +1,13 @@
+package com.lwj.projectampq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectAmpqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectAmpqApplication.class, args);
+    }
+
+}
---
+++ project-es/src/main/java/com/lwj/projectes/ProjectEsApplication.java
@@ -0,0 +1,13 @@
+package com.lwj.projectes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectEsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectEsApplication.class, args);
+    }
+
+}
